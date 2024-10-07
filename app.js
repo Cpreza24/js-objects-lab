1863551d99d14f36f8ae43a1810c78b243668bea
Cesar Preza <cpreza93@yahoo.com>
2024-10-06 20:15:54 -0700

    completed js objects lab

--- app.js
+++ app.js
@@ -271,3 +271,28 @@ game.partyCount = function() {
 
 game.partyCount();
 
+/*
+Exercise 15
+1. Now, complete gyms with a difficulty below 8. Reflect on how this is similar to or different from the previous gym exercises.
+(change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 15 here:
+*/
+
+for (let i = 0; i < game.gyms.length; i++) {
+    if (game.gyms[i].difficulty < 8) {
+        game.gyms[i].completed = true;
+    }
+}
+
+
+
+/*
+Exercise 16
+1. Log the entire `game` object to the console. Take a moment to review the changes you've made throughout the exercises.
+
+
+Solve Exercise 16 here:
+*/
+
+console.log(game);
